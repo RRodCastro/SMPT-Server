a268f520f76b7e7f7acc680e897052643c7281de
juiceHN <13341708+juiceHN@users.noreply.github.com>
2018-02-13 21:30:25 -0600

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,21 +1,10 @@
-import re
 from tkinter import *
-# import pymongo
-# client = pymongo.MongoClient()
-# db = client.
+from pymongo import *
 
-
-
-
-def verCorreo(correo):
-    p = re.compile('gmail|hotmail|yahoo|uvg')
-    a = p.search(correo)
-    if a:
-        print('correo valido')
-        return True
-    else:
-        print('correo invalido')
-        return False
+client = MongoClient()
+lista = []
+# cambiar la base de datos
+# db = client.todos
 
 
 class Login(Tk):
@@ -25,6 +14,9 @@ class Login(Tk):
         self.config(bg="grey")
 
         user = StringVar()
+        table_top = StringVar()
+
+        table_top.set('')
 
         Label(self, text="User: ").grid(row=2, column=0, sticky=W)
         self.profile = Entry(self, textvariable=user, width=50)
@@ -36,46 +28,38 @@ class Login(Tk):
         self.exit_button = Button(
             self, text="Exit", command=self.quit, bg="red")
         self.exit_button.grid(row=6, column=0)
+        self.mail = Button(self, text="Correos", command=self.count_Mail)
+        self.mail.grid(row=7, column=0)
+        Label(self, text=table_top).grid(row=8, column=0, sticky=W)
 
     def enter(self):
         self.user = self.profile.get()
-        print("user: " + self.user)
+        # print("user: " + self.user)
+        print("S: +OK")
 
-        correo = self.user + "@lostukus.com"
-
-        print ("correo: " + correo)
-
-
-class Otro(Tk):
-    def __init__(self, master=None):
-        Tk.__init__(self)
-        self.title("Simple Login")
-        self.config(bg="red")
-
-        user = StringVar()
-
-        Label(self, text="User: ").grid(row=2, column=0, sticky=W)
-        self.profile = Entry(self, textvariable=user, width=50)
-        self.profile.grid(row=3, column=0, sticky=W)
-
-        self.login_button = Button(
-            self, text="Log in", command=self.enter, bg="green")
-        self.login_button.grid(row=5, column=0)
-        self.exit_button = Button(
-            self, text="Exit", command=self.quit, bg="red")
-        self.exit_button.grid(row=6, column=0)
-
-    def enter(self):
+    # funcion para buscar mail de la DB
+    def fetch_Mail(self):
+        table_top = "No. |   From   |   Subject   |   Data    | \n"
+        contador = 0
         self.user = self.profile.get()
-        print("user: " + self.user)
+        correos = db.bandeja_entrada.find({"To": self.user})
+        for i in correos:
+            contador += 1
+            a = i['From']
+            b = i['Subject']
+            c = i['Data']
+            # lista.append(i)
+            table_top = table_top + "   " + \
+                str(contador) + "| " + a + " | " + b + " | " + c + "\n"
+        print (table_top)
 
-        correo = self.user + "@lostukus.com"
-
-        print ("correo: " + correo)
+    # funcion para contar correos
+    def count_Mail(self):
+        self.user = self.profile.get()
+        no_correos = db.bandeja_entrada.find({"To": self.user}).count()
+        print("OK " + self.user + "'s maildrop has " +
+              str(no_correos) + " messages")
 
 
 app = Login()
-app.mainloop()
-
-app = Otro()
 app.mainloop()
