7e48f6481c3889d39bd10d7d2ca9041605082656
RRodCastro <mrcastro9574@gmail.com>
2018-03-02 18:20:25 -0600

    Add threadpool and refactor

--- SMTP.py
+++ SMTP.py
@@ -1,21 +1,70 @@
-from socket import socket, AF_INET, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_STREAM, gethostname, gethostbyname
 import sys
 import threading
 import re
 from multiprocessing import pool
 from DB import DataBase
 import time
+from threading import Thread
+from queue import Queue
+
+
+class Worker(Thread):
+    """ Thread executing tasks from a given tasks queue """
+
+    def __init__(self, tasks):
+        Thread.__init__(self)
+        self.tasks = tasks
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            func, args, kargs = self.tasks.get()
+            try:
+                func(*args, **kargs)
+            except Exception as e:
+                # An exception happened in this thread
+                print(e)
+            finally:
+                # Mark this task as done, whether an exception happened or not
+                self.tasks.task_done()
+
+
+class ThreadPool:
+    """ Pool of threads consuming tasks from a queue """
+
+    def __init__(self, num_threads):
+        self.tasks = Queue(num_threads)
+        for _ in range(num_threads):
+            Worker(self.tasks)
+
+    def add_task(self, func, *args, **kargs):
+        """ Add a task to the queue """
+        self.tasks.put((func, args, kargs))
+
+    def map(self, func, args_list):
+        """ Add a list of tasks to the queue """
+        for args in args_list:
+            self.add_task(func, args)
+
+    def wait_completion(self):
+        """ Wait for completion of all the tasks in the queue """
+        self.tasks.join()
 
 
 class SMTPServer:
-    def __init__(self, server_socket):
-        self.socket = server_socket
-        self.domain = "gmail.com"
+    pool = ThreadPool(5)
 
-    def process_command(self, regex):
+    def __init__(self):
+        self.smtp_port = 8080
+        self.domain = "grupo01.com"
+        self.host = gethostbyname(gethostname())
+
+    def process_command(self, regex, socket):
         recive_data = ""
         while True:
-            data = self.socket.recv(1024).decode()
+            data = socket.recv(1024).decode()
             recive_data = recive_data + data
             if("\r\n" in recive_data):
                 break
@@ -26,39 +75,39 @@ class SMTPServer:
         else:
             send_data = ("ERROR")
             print("S: " + send_data)
-            self.socket.send(str.encode(send_data))
+            socket.send(str.encode(send_data))
             return False
 
-    def process_message(self):
+    def process_message(self, socket):
         recive_data = ""
         while True:
-            data = self.socket.recv(1024).decode()
+            data = socket.recv(1024).decode()
             recive_data = recive_data + data
             if("\r\n" in recive_data):
                 break
         return recive_data.split("\r\n")[0]
 
-    def proces_Helo(self):
-        self.socket.send(str.encode("220 " + self.domain + "\r\n"))
+    def proces_Helo(self, socket):
+        socket.send(str.encode("220 " + self.domain + "\r\n"))
         print("S: 220 " + self.domain)
         regex_helo = re.compile(r"HELO (\w+)(\.)(\w+)")
-        match = self.process_command(regex_helo)
+        match = self.process_command(regex_helo, socket)
         if (match):
             send_data = ("250 HELLO " + match.group(1) + "." +
                          match.group(3) + ", pleased to meet you\r\n")
-            self.socket.send(str.encode(send_data))
+            socket.send(str.encode(send_data))
             print("S: " + send_data)
             return True
         else:
             return False
 
-    def proces_from(self):
+    def proces_from(self, socket):
         regex_from = re.compile(r"MAIL FROM: <(\w+)(@)(\w+)(\.)(\w+)>")
-        match = self.process_command(regex_from)
+        match = self.process_command(regex_from, socket)
         if(match):
             send_data = ("250 " + match.group(1) + match.group(2) +
                          match.group(3) + match.group(4) + match.group(5) + " ... sender Ok\r\n")
-            self.socket.send(str.encode(send_data))
+            socket.send(str.encode(send_data))
             self.mail_from = match.group(
                 1) + match.group(2) + match.group(3) + match.group(4) + match.group(5)
             print("S: " + send_data)
@@ -66,13 +115,13 @@ class SMTPServer:
         else:
             return False
 
-    def process_to(self):
+    def process_to(self, socket):
         regex_to = re.compile(r"RCPT TO: <(\w+)(@)(\w+)(\.)(\w+)>")
-        match = self.process_command(regex_to)
+        match = self.process_command(regex_to, socket)
         if(match):
             send_data = ("250 " + match.group(1) + match.group(2) + match.group(3) +
                          match.group(4) + match.group(5) + " ... recipent Ok\r\n")
-            self.socket.send(str.encode(send_data))
+            socket.send(str.encode(send_data))
             self.mail_to = match.group(
                 1) + match.group(2) + match.group(3) + match.group(4) + match.group(5)
             print("S: " + send_data)
@@ -80,44 +129,55 @@ class SMTPServer:
         else:
             return False
 
-    def process_data(self):
+    def process_data(self, socket):
         regex_data = re.compile(r"(DATA)")
-        match = self.process_command(regex_data)
+        match = self.process_command(regex_data, socket)
         if (match):
             send_data = "354 Enter mail, end with “.” on a line by itself\r\n"
-            self.socket.send(str.encode(send_data))
+            socket.send(str.encode(send_data))
             print("S: " + send_data)
-            recive_data = self.process_message()
+            recive_data = self.process_message(socket)
             message = recive_data
             while(recive_data != '.'):
-                recive_data = self.process_message()
+                recive_data = self.process_message(socket)
                 message += recive_data + "\n"
             print("C: " + message)
             send_data = "250 Message accepted for delivery\r\n"
             self.send_data = message
-            self.socket.send(str.encode(send_data))
+            socket.send(str.encode(send_data))
             print("S: " + send_data)
             return True
         else:
             return False
 
-    def process_quit(self):
+    def process_quit(self, socket):
         regex_quit = re.compile(r"(QUIT)")
-        match = self.process_command(regex_quit)
+        match = self.process_command(regex_quit, socket)
         if(match):
             send_data = "221 hamburger.edu closing connection\r\n"
-            self.socket.send(str.encode(send_data))
+            socket.send(str.encode(send_data))
             print("S: " + send_data)
         else:
             False
 
-    def send_and_recive(self):
-        if (self.proces_Helo()):
-            if(self.proces_from()):
-                if(self.process_to()):
-                    if(self.process_data()):
-                        if(self.process_quit()):
-                            return True
+    def send_and_recive(self, client_socket, client_address):
+        if (self.proces_Helo(client_socket)):
+            if(self.proces_from(client_socket)):
+                if(self.process_to(client_socket)):
+                    if(self.process_data(client_socket)):
+                        if(self.process_quit(client_socket)):
+                            self.handle_mail(client_socket)
+
+    def handle_mail(self, socket):
+        domain = self.mail_to.split("@")[1].replace(">", "")
+        print(self.send_data)
+        if(domain == self.domain):
+            database = DataBase()
+            database.save_Mail(
+                self.mail_from, self.mail_to, self.send_data)
+        else:
+            print("sending to..." + domain)
+            self.send_mail(domain)
 
     def recive_command(self, clientServer):
         recive_data = ""
@@ -173,30 +233,25 @@ class SMTPServer:
         print("S: " + self.recive_command(clientServer))
         clientServer.close()
 
-    def handle_clients(self, serverSocket):
+    def handle_clients(self, serverSocket, task):
+        serverSocket.listen(5)
         while (True):
             try:
-                print("Waiting clients...")
-                connectionSocket, addr = serverSocket.accept()
-                self.socket = connectionSocket
-                self.send_and_recive()
-                domain = self.mail_to.split("@")[1].replace(">", "")
-                print(self.send_data)
-                if(domain == self.domain):
-                    database = DataBase()
-                    database.save_Mail(
-                        self.mail_from, self.mail_to, self.send_data)
-                else:
-                    print("sending to..." + domain)
-                    self.send_mail(domain)
+                client_socket, client_address = serverSocket.accept()
+                self.pool.add_task(
+                    self.send_and_recive, client_socket, client_address)
             except KeyboardInterrupt:
-                self.socket.close()
+                client_socket.close()
                 break
 
+    def runnable(self):
+        serverSocket = socket(AF_INET, SOCK_STREAM)
+        serverSocket.bind((self.host, self.smtp_port))
+        print("SMTP running on: ", self.host, self.smtp_port)
+        server_thread = Thread(target=self.handle_clients,
+                               args=(serverSocket, "smtp"))
+        server_thread.start()
 
-serverPort = 8080
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('127.0.0.1', serverPort))
-serverSocket.listen(5)
-server = SMTPServer(serverSocket)
-server.handle_clients(serverSocket)
+
+server = SMTPServer()
+server.runnable()
--- pop3-server.py
+++ pop3-server.py
@@ -1,20 +1,67 @@
-from socket import socket, AF_INET, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_STREAM, gethostname, gethostbyname
 from DB import DataBase
 import re
 import time
+from threading import Thread
+from queue import Queue
+
+
+class Worker(Thread):
+    """ Thread executing tasks from a given tasks queue """
+
+    def __init__(self, tasks):
+        Thread.__init__(self)
+        self.tasks = tasks
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            func, args, kargs = self.tasks.get()
+            try:
+                func(*args, **kargs)
+            except Exception as e:
+                # An exception happened in this thread
+                print(e)
+            finally:
+                # Mark this task as done, whether an exception happened or not
+                self.tasks.task_done()
+
+
+class ThreadPool:
+    """ Pool of threads consuming tasks from a queue """
+
+    def __init__(self, num_threads):
+        self.tasks = Queue(num_threads)
+        for _ in range(num_threads):
+            Worker(self.tasks)
+
+    def add_task(self, func, *args, **kargs):
+        """ Add a task to the queue """
+        self.tasks.put((func, args, kargs))
+
+    def map(self, func, args_list):
+        """ Add a list of tasks to the queue """
+        for args in args_list:
+            self.add_task(func, args)
+
+    def wait_completion(self):
+        """ Wait for completion of all the tasks in the queue """
+        self.tasks.join()
 
 
 class POP3Server:
-    def __init__(self, server_socket):
-        self.socket = server_socket
-        self.database = DataBase()
-        self.logged_in = False
+    pool = ThreadPool(5)
 
-    def process_command(self, regex):
+    def __init__(self):
+        self.smtp_port = 8085
+        self.database = DataBase()
+        self.host = gethostbyname(gethostname())
+
+    def process_command(self, regex, socket):
         recive_data = ""
         while True:
-            data = self.socket.recv(1024).decode()
-            print(data)
+            data = socket.recv(1024).decode()
             recive_data = recive_data + data
             if("\r\n" in recive_data):
                 break
@@ -25,113 +72,119 @@ class POP3Server:
         else:
             send_data = ("-ERR\r\n")
             print("S: " + send_data)
-            self.socket.send(str.encode(send_data))
+            socket.send(str.encode(send_data))
             return False
 
-    def authorization_phase(self):
-        self.socket.send("+OK POP3 server ready\r\n".encode())
+    def authorization_phase(self, socket):
+        user_and_password = False
+        socket.send("+OK POP3 server ready\r\n".encode())
         print("S: +OK POP3 server ready")
-        regex_user = re.compile(r"user (.+)")
-        match = self.process_command(regex_user)
+        regex_user = re.compile(r"USER (.+)")
+        match = self.process_command(regex_user, socket)
         if(match):
-            self.user = match.group(1)
-            user = self.database.fetchUser(self.user)
+            user_name = match.group(1)
+            user = self.database.fetchUser(user_name)
             while(not user):
-                self.socket.send("-ERR\r\n".encode())
-                regex_user = re.compile(r"user (.+)")
-                match = self.process_command(regex_user)
+                socket.send("-ERR\r\n".encode())
+                regex_user = re.compile(r"USER (.+)")
+                match = self.process_command(regex_user, socket)
                 if(match):
-                    self.user = match.group(1)
-                    user = self.database.fetchUser(self.user)
+                    user_name = match.group(1)
+                    user = self.database.fetchUser(user_name)
         if (user):
-            self.socket.send("+OK\r\n".encode())
+            socket.send("+OK\r\n".encode())
             print("C: +OK")
-            regex_password = re.compile(r"pass (\w+)")
-            match = self.process_command(regex_password)
+            regex_password = re.compile(r"PASS (.+)")
+            match = self.process_command(regex_password, socket)
             if(match):
-                self.password = match.group(1)
+                password = match.group(1)
                 user_and_password = self.database.fetch_user_password(
-                    self.user, self.password)
+                    user_name, password)
                 while(not user_and_password):
-                    self.socket.send("-ERR\r\n".encode())
-                    regex_password = re.compile(r"pass (\w+)")
-                    match = self.process_command(regex_password)
+                    socket.send("-ERR\r\n".encode())
+                    regex_password = re.compile(r"PASS (\w+)")
+                    match = self.process_command(regex_password, socket)
                     if(match):
-                        self.password = match.group(1)
+                        password = match.group(1)
                         user_and_password = self.database.fetch_user_password(
-                            self.user, self.password)
-        if (user_and_password):
-            self.socket.send(
+                            user_name, password)
+        if (user_and_password, socket):
+            socket.send(
                 "+OK user successfully logged on\r\n".encode())
             print("+OK user successfully logged on")
-            self.logged_in = True
             return True
         else:
             return False
 
-    def recive_command(self):
+    def recive_command(self, socket):
         recive_data = ""
         while True:
-            data = self.socket.recv(1024).decode()
+            data = socket.recv(1024).decode()
             recive_data = recive_data + data
             if("\r\n" in recive_data):
                 break
         return recive_data
 
-    def retr_message(self, message_list):
+    def retr_message(self, message_list, socket):
         for i in enumerate(message_list):
-            regex_retr = re.compile(r"retr (\d+)")
-            match = self.process_command(regex_retr)
+            regex_retr = re.compile(r"RETR | retr (\d+)")
+            match = self.process_command(regex_retr, socket)
             if (match):
                 index = (message_list[int(match.group(1))-1])
-                self.socket.send(str(index['From'] + "\r\n").encode())
+                socket.send(str(index['From'] + "\r\n").encode())
                 time.sleep(0.2)
-                self.socket.send(str(index['Data'] + "\r\n").encode())
-                self.socket.send(".\r\n".encode())
+                socket.send(str(index['Data'] + "\r\n").encode())
+                socket.send(".\r\n".encode())
                 print("S: .")
                 time.sleep(0.2)
 
-    def transaction_phase(self):
-        regex_list = re.compile(r"list")
-        match = self.process_command(regex_list)
+    def transaction_phase(self, socket):
+        regex_list = re.compile(r"LIST | list")
+        match = self.process_command(regex_list, socket)
         if(match):
             message_list = (self.database.fetch_Mail("mailto@gmail.com"))
             for i, mail in enumerate(message_list):
-                self.socket.send(
+                socket.send(
                     str(str(i+1) + " " + str(len((mail['Data'].encode()))) + "\r\n").encode())
                 print("S: " + str(str(i) + " " +
                                   str(len((mail['Data'].encode())))))
                 time.sleep(0.1)
-            self.socket.send(".\r\n".encode())
+            socket.send(".\r\n".encode())
             print("S: .")
-            self.retr_message(message_list)
+            self.retr_message(message_list, socket)
             # C: dele 1
 
-    def quit_phase(self):
+    def quit_phase(self, socket):
         regex_list = re.compile(r"quit")
-        match = self.process_command(regex_list)
+        match = self.process_command(regex_list, socket)
         if(match):
-            self.socket.send("+OK POP3 server signing off\r\n".encode())
+            socket.send("+OK POP3 server signing off\r\n".encode())
 
-    def handle_clients(self, serverSocket):
+    def phases(self, socket, socket_address):
+        self.authorization_phase(socket)
+        self.transaction_phase(socket)
+        self.quit_phase(socket)
+        socket.close()
+
+    def handle_clients(self, serverSocket, task):
+        serverSocket.listen(5)
         while(True):
             try:
-                print("Waiting clients...")
-                connectionSocket, addr = serverSocket.accept()
-                if (not self.logged_in):
-                    self.socket = connectionSocket
-                self.authorization_phase()
-                self.transaction_phase()
-                self.quit_phase()
-                self.socket.close()
+                client_socket, client_address = serverSocket.accept()
+                self.pool.add_task(
+                    self.phases, client_socket, client_address)
             except KeyboardInterrupt:
-                self.socket.close()
+                client_socket.close()
                 break
 
+    def runnable(self):
+        serverSocket = socket(AF_INET, SOCK_STREAM)
+        serverSocket.bind((self.host, self.smtp_port))
+        print("POP3 running on: ", self.host, self.smtp_port)
+        server_thread = Thread(target=self.handle_clients,
+                               args=(serverSocket, "pop3"))
+        server_thread.start()
 
-serverPort = 8081
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('127.0.0.1', serverPort))
-serverSocket.listen(5)
-server = POP3Server(serverSocket)
-server.handle_clients(serverSocket)
+
+server = POP3Server()
+server.runnable()
